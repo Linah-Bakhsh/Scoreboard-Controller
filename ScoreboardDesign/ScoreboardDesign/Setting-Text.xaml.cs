655a4b1efee0ad0b3272146743f6f8f91c2e9c98
Linah-Bakhsh <you@example.com>
2022-04-11 12:33:37 +0300

    test

---
+++ ScoreboardDesign/ScoreboardDesign/Setting-Text.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScoreboardDesign
+{
+    /// <summary>
+    /// Interaction logic for Setting_Text.xaml
+    /// </summary>
+    public partial class Setting_Text : Page
+    {
+        public Setting_Text()
+        {
+            InitializeComponent();
+        }
+    }
+}
